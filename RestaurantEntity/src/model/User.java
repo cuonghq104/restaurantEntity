77676d7bbc18756c6dd84064b8f40a72604a7b8f
Duc Nguyen <ceasarjulius62@gmail.com>
2017-11-11 11:19:13 +0700

    class Account, Employee, Customer, User added

---
+++ RestaurantEntity/src/model/User.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Dell
+ */
+public class User implements Serializable {
+
+    private int id;
+    private String name;
+    private String address;
+    private String tel;
+    private String email;
+    private Account account;
+
+    public User() {
+    }
+
+    public User(int id, String name, String address, String tel, String email, Account account) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.tel = tel;
+        this.email = email;
+        this.account = account;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+}
